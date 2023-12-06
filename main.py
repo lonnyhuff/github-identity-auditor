cd89d7da55b6da577578737bf7d4f9875baada0f
Lonny <lonnyhuff@gmail.com>
2023-12-06 12:27:07 -0500

    Add error handling and debugging for GraphQL queries in identity fetch script

--- main.py
+++ main.py
@@ -47,33 +47,38 @@ def fetch_linked_users(org, token):
         "Content-Type": "application/json"
     }
 
-    # Initialize pagination
+        # Pagination initialization
     has_next_page = True
     cursor = None
 
-    # Loop through pages of results
+    # Fetching data with pagination
     while has_next_page:
         variables = {"org": org, "cursor": cursor}
         response = requests.post(url, headers=headers, json={'query': query, 'variables': variables})
         
-        # Check for request failure
         if response.status_code != 200:
             raise Exception(f"Query failed with status code {response.status_code}: {response.text}")
 
         data = response.json()
-        organization_data = data['data']['organization']['samlIdentityProvider']['externalIdentities']
-        
-        # Process and print user data
+
+        # Debugging log
+        print(json.dumps(data, indent=4))
+
+        # Check if the data contains the expected path
+        if data.get('data') and data['data'].get('organization') and data['data']['organization'].get('samlIdentityProvider') and data['data']['organization']['samlIdentityProvider'].get('externalIdentities'):
+            organization_data = data['data']['organization']['samlIdentityProvider']['externalIdentities']
+        else:
+            print("Data structure does not match expected format.")
+            return
+
         for edge in organization_data['edges']:
             user = edge['node']
             print(f"Login: {user['user']['login']}, SAML Name ID: {user['samlIdentity']['nameId']}, SCIM Username: {user['scimIdentity']['username']}")
 
-        # Update cursor for next page
         has_next_page = organization_data['pageInfo']['hasNextPage']
         cursor = organization_data['pageInfo']['endCursor']
 
 if __name__ == "__main__":
-    # Check for correct number of arguments
     if len(sys.argv) != 3:
         print("Usage: python script.py <organization_name> <personal_access_token>")
         sys.exit(1)
